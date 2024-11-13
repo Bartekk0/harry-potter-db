40bd7c4a48b2b251b90c3f3c130e2c8bad34efe3
Bartek Wątorek <bwatorek86@uczen.tl.krakow.pl>
2024-11-13 18:34:43 +0100

    Color scheme generated (initially)

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,4 +3,85 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-export const Colors = {};
+export const Colors = {
+  gryffindor: {
+    light: {
+      primary: "#740001",
+      secondary: "#eeba30",
+      tertiary: "#d3a625",
+      quaternary: "#000000",
+      quinary: "#ffffff",
+      bg: "#f7f7f7",
+      text: "#000000",
+    },
+    dark: {
+      primary: "#740001",
+      secondary: "#eeba30",
+      tertiary: "#d3a625",
+      quaternary: "#000000",
+      quinary: "#ffffff",
+      bg: "#000000",
+      text: "#ffffff",
+    },
+  },
+  ravenclaw: {
+    light: {
+      primary: "#222f5b",
+      secondary: "#946b2d",
+      tertiary: "#000000",
+      quaternary: "#946b2d",
+      quinary: "#ffffff",
+      bg: "#f7f7f7",
+      text: "#000000",
+    },
+    dark: {
+      primary: "#222f5b",
+      secondary: "#946b2d",
+      tertiary: "#000000",
+      quaternary: "#946b2d",
+      quinary: "#ffffff",
+      bg: "#000000",
+      text: "#ffffff",
+    },
+  },
+  hufflepuff: {
+    light: {
+      primary: "#000000",
+      secondary: "#eeba30",
+      tertiary: "#000000",
+      quaternary: "#000000",
+      quinary: "#ffffff",
+      bg: "#f7f7f7",
+      text: "#000000",
+    },
+    dark: {
+      primary: "#000000",
+      secondary: "#eeba30",
+      tertiary: "#000000",
+      quaternary: "#000000",
+      quinary: "#ffffff",
+      bg: "#000000",
+      text: "#ffffff",
+    },
+  },
+  slytherin: {
+    light: {
+      primary: "#1a472a",
+      secondary: "#aaaaaa",
+      tertiary: "#000000",
+      quaternary: "#aaaaaa",
+      quinary: "#ffffff",
+      bg: "#f7f7f7",
+      text: "#000000",
+    },
+    dark: {
+      primary: "#1a472a",
+      secondary: "#aaaaaa",
+      tertiary: "#000000",
+      quaternary: "#aaaaaa",
+      quinary: "#ffffff",
+      bg: "#000000",
+      text: "#ffffff",
+    },
+  },
+};
